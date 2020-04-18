9930a04e96ff174eb1e0c6acc9108c5ae202189d
Jan Kroeze <jcwkroeze@protonmail.com>
2020-04-18 15:22:02 +0200

    This can be undefined

--- src/VulkanApplication.cpp
+++ src/VulkanApplication.cpp
@@ -434,7 +434,7 @@ createRenderPass() {
     colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
     colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
     colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
     colorAttachment.finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
     attachmentDescriptions.push_back(colorAttachment);
 
