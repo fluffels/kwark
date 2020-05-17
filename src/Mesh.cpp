43ff6877b7921b56290ecfa8901569f807b53aa1
Jan Kroeze <jcwkroeze@protonmail.com>
2020-05-17 17:21:20 +0200

    Get lightmap per vertex

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -7,14 +7,13 @@ using glm::dot;
 using glm::normalize;
 using glm::vec2;
 
-vec2 calculateTexCoord(
+vec2 calculateUV(
     vec3& vertex,
-    TexInfo& texInfo,
-    TextureHeader& texHeader
+    TexInfo& texInfo
 ) {
     vec2 result = {
-        (dot(vertex, texInfo.uVector) + texInfo.uOffset) / texHeader.width,
-        (dot(vertex, texInfo.vVector) + texInfo.vOffset) / texHeader.height
+        (dot(vertex, texInfo.uVector) + texInfo.uOffset),
+        (dot(vertex, texInfo.vVector) + texInfo.vOffset)
     };
     return result;
 }
@@ -27,7 +26,7 @@ Mesh::Mesh(BSPParser& bsp):
 
 void Mesh::buildWireFrameModel() {
     for (Face& face: bsp.faces) {
-        vector<vec3> faceVertices;
+        vector<vec3> faceCoords;
         auto edgeListBaseId = face.ledgeId;
         for (uint32_t i = 0; i < face.ledgeNum; i++) {
             auto edgeListId = edgeListBaseId + i;
@@ -36,24 +35,15 @@ void Mesh::buildWireFrameModel() {
             vec3 v0 = bsp.vertices[edge.v0];
             vec3 v1 = bsp.vertices[edge.v1];
             if (edgeId < 0) {
-                faceVertices.push_back(v1);
-                faceVertices.push_back(v0);
+                faceCoords.push_back(v1);
+                faceCoords.push_back(v0);
             } else if (edgeId > 0) {
-                faceVertices.push_back(v0);
-                faceVertices.push_back(v1);
+                faceCoords.push_back(v0);
+                faceCoords.push_back(v1);
             }
         }
 
         Vertex v0, v1, v2;
-
-        auto light = 1.f - face.baseLight / 255.0f;
-        if (face.lightmap >= 0) {
-            auto lightMap = bsp.lightMap[face.lightmap] / 255.0f;
-            light += lightMap;
-        }
-        v0.light = light;
-        v1.light = light;
-        v2.light = light;
 
         auto& texInfo = bsp.texInfos[face.texinfoId];
         auto& texHeader = bsp.atlas->textureHeaders[texInfo.textureID];
@@ -63,20 +53,55 @@ void Mesh::buildWireFrameModel() {
         v1.texIdx = texNum;
         v2.texIdx = texNum;
 
-        auto& p0 = faceVertices[0];
+        auto& p0 = faceCoords[0];
         v0.pos = p0;
-        v0.texCoord = calculateTexCoord(v0.pos, texInfo, texHeader);
+        v0.texCoord = calculateUV(v0.pos, texInfo);
 
+        vector<Vertex> faceVertices;
         for (uint32_t i = 1; i < face.ledgeNum; i++) {
-            vertices.push_back(v0);
+            faceVertices.push_back(v0);
 
-            v1.pos = faceVertices[i*2];
-            v1.texCoord = calculateTexCoord(v1.pos, texInfo, texHeader);
-            vertices.push_back(v1);
+            v1.pos = faceCoords[i*2];
+            v1.texCoord = calculateUV(v1.pos, texInfo);
+            faceVertices.push_back(v1);
 
-            v2.pos = faceVertices[i*2+1];
-            v2.texCoord = calculateTexCoord(v2.pos, texInfo, texHeader);
-            vertices.push_back(v2);
+            v2.pos = faceCoords[i*2+1];
+            v2.texCoord = calculateUV(v2.pos, texInfo);
+            faceVertices.push_back(v2);
+        }
+
+        vec2 uvMin = faceVertices[0].texCoord;
+        vec2 uvMax = faceVertices[0].texCoord;
+        for (auto& v: faceVertices) {
+            auto& uv = v.texCoord;
+            if (uv.x < uvMin.x) uvMin.x = uv.x;
+            if (uv.y < uvMin.y) uvMin.y = uv.y;
+            if (uv.x > uvMax.x) uvMax.x = uv.x;
+            if (uv.y > uvMax.y) uvMax.y = uv.y;
+        }
+        auto texelWidth = int(uvMax.x - uvMin.x) / 16;
+        auto texelHeight = int(uvMax.y - uvMin.y) / 16;
+
+        auto baseLight = 1.f - face.baseLight / 255.f;
+        for (auto& vertex: faceVertices) {
+            if (face.lightmap >= 0) {
+                auto& uv = vertex.texCoord;
+                auto u = int(uv.x - uvMin.x) / 16;
+                auto v = int(uv.y - uvMin.y) / 16;
+                auto lightIdx = face.lightmap + v*texelWidth + u;
+                auto lightMap = bsp.lightMap[lightIdx];
+                vertex.light = baseLight + lightMap / 255.f;
+            }
+        }
+
+        for (auto& v: faceVertices) {
+            auto& uv = v.texCoord;
+            uv.x /= texHeader.width;
+            uv.y /= texHeader.height;
+        }
+
+        for (auto& v: faceVertices) {
+            vertices.push_back(v);
         }
     }
 }
